70e56b8485740e8c05b27f6524f3d18427bdcfba
HARSH RAJ <122086619+Harshraj843112@users.noreply.github.com>
2025-03-29 19:50:54 +0530

    changes config file

--- my-project/src/config.js
+++ my-project/src/config.js
@@ -1,8 +1,8 @@
 const isProduction = process.env.NODE_ENV === "production";
 
 const config = {
-    apiBaseUrl: isProduction ? "https://your-production-api.com" : "http://localhost:5000",
-    socketBaseUrl: isProduction ? "https://your-production-socket.com" : "http://localhost:5000",
+    apiBaseUrl: isProduction ? "https://gudmed-backend.onrender.com" : "http://localhost:5000",
+    socketBaseUrl: isProduction ? "https://gudmed-backend.onrender.com" : "http://localhost:5000",
 };
 
 export default config;
